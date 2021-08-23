f67cec9f56972aab83655f4a96f72053a45d1366
micahabanschick <micha.ban@gmail.com>
2021-08-22 23:19:19 -0400

    loop blocks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header'
 import Footer from './components/footer'
+import { BlocksContainer } from './containers/blocks_container';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <Header/>
+      <BlocksContainer/>
       <Footer/>
     </div>
   );
--- src/components/blocks.js
+++ src/components/blocks.js
@@ -2,10 +2,27 @@ import React, { Component } from 'react'
 import Block from './block'
 
 export default class Blocks extends Component {
+
+    state = {"1": 1,"2": 2,"3": 3}
+
     render() {
+        // let i = 0
+        // while (i < 4) {
+        //     i++
+        //     return (
+        //         <div>
+        //             <Block/>
+        //         </div>
+        //     )
+        // }
+
         return (
+        
             <div>
-                <Block/>
+                
+                {[1,2,3,4,5].map((value, index) => (
+                    <Block id={index + 1} key={index} />
+                ))}
             </div>
         )
     }
