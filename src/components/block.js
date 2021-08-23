225a4534e520a9a514d70cdbd1a8d0bfa96e4662
micahabanschick <micha.ban@gmail.com>
2021-08-23 12:34:36 -0400

    .App-block

--- src/components/block.js
+++ src/components/block.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export class Block extends Component {
     render() {
         return (
-            <div>
+            <div className="App-block">
                 <h1>THIS IS A BLOCK</h1>
             </div>
         )
