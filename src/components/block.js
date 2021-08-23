7d05b5961d3149e6b508f6122bc6d6e05dbb7569
micahabanschick <micha.ban@gmail.com>
2021-08-23 12:47:36 -0400

    placeholder img in block

--- src/components/block.js
+++ src/components/block.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import placeholderImage from '../assets/images/placeholder.png'
 
 export class Block extends Component {
     render() {
         return (
             <div className="App-block">
                 <h1>THIS IS A BLOCK</h1>
+                <img src={placeholderImage} alt="placeholder img"></img>
             </div>
         )
     }
